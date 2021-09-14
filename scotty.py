8d12ba2fa5eeb34f342484329b0f0f62d677649d
Bruno Dooms <bruno.dooms@kuleuven.be>
2021-09-14 18:31:00 +0200

    Add dynamic config options

--- scotty.py
+++ scotty.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 
 import argparse
+import glob
 import inquirer
 import os
 # from pprint import pprint
@@ -8,19 +9,76 @@ import re
 import sys
 import yaml
 
+appname = 'scotty'
+
 # -----------------------------------------------
 # Arguments
 # -----------------------------------------------
-parser = argparse.ArgumentParser('Cd into remote directories...')
+parser = argparse.ArgumentParser('Beam me up to remote directories...')
 
-parser.add_argument('-c', '--configfile', help='config yaml file', required=True)
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('-c', '--configfile', help='config yaml file', required=False)
+group.add_argument('-b', '--browse', help='browse through all available configs', required=False, action='store_true')
+group.add_argument('-d', '--configdir', help='browse through given config dir', required=False)
 
-parser.add_argument('--interactive', '-i', help='use server aliases', required=False, action='store_true')
+parser.add_argument('--interactive', '-i', help='use interactive mode', required=False, action='store_true')
 
 args = parser.parse_args()
 
 # -----------------------------------------------
-# Valdate
+# Get config file
+# -----------------------------------------------
+homedir = os.getenv("HOME")
+config_file = args.configfile
+
+if args.configfile:
+    if re.match("^~\/", config_file):
+        file_path = os.path.abspath(config_file.replace('~', homedir))
+    elif re.match("^\.\/", config_file) or re.match("^\/", config_file):
+        file_path = os.path.abspath(config_file)
+    else:
+        file_path = os.path.abspath('./' + config_file)
+else:
+    # search paths
+    abspath = os.path.abspath(__file__)
+    dname = os.path.dirname(abspath)
+
+    paths_found = []
+
+    if args.configdir:
+        paths_search = [args.configdir]
+    else:
+        confdir_current = dname + '/conf.d/'
+        confdir_home = os.path.abspath(homedir) + f'/.{appname}/conf.d/'
+        confdir_global = f'/etc/{appname}/conf.d/'
+
+        paths_search = [confdir_home, confdir_global]
+
+    for path in paths_search:
+        if os.path.exists(path):
+            paths_found.append(path)
+
+    paths_search_display = ', '.join(paths_search)
+
+    if not len(paths_found):
+        sys.exit(f'No config dirs found: {paths_search_display}')
+
+    config_files = []
+    for path in paths_found:
+        config_files += glob.glob(f"{path}*.yml")
+
+    if not len(config_files):
+        sys.exit(f'No config files found: {paths_search_display}')
+
+    questions = [inquirer.List('select_path', message=f'Select config file:', choices=config_files,),]
+    answers = inquirer.prompt(questions)
+    file_path = answers['select_path']
+
+if not os.path.exists(file_path):
+    sys.exit(f'Config file "{file_path}" not found!')
+
+# -----------------------------------------------
+# Valdate config file
 # -----------------------------------------------
 file_path = os.path.abspath(args.configfile)
 if not os.path.exists(file_path):
